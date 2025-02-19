ff74a6ba0bd58435baf81d5f0144b3ec4edc7d88
John Smith <browntruck246@gmail.com>
2025-02-19 16:24:18 -0500

    Update protype.sql

--- protype2/protype.sql
+++ protype2/protype.sql
@@ -1,18 +1,3 @@
-CREATE TABLE `protype`.`customer` (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL
-);
-
-INSERT INTO `protype`.`customer` (first_name, last_name) VALUES ('John', 'Doe');
-INSERT INTO `protype`.`customer` (first_name, last_name) VALUES ('Jane', 'Smith');
-
-SELECT * FROM `protype`.`customer`;
-
-SELECT * FROM protype.customer;
-SELECT first_name, last_name FROM protype.customer where customer_id = 1;
-
-
 CREATE TABLE `protype`.`product` (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -25,5 +10,7 @@ INSERT INTO `protype`.`product` (name, description, price) VALUES ('Banana', 'A 
 INSERT INTO `protype`.`product` (name, description, price) VALUES ('Orange', 'A sweet orange orange', 0.60);
 INSERT INTO `protype`.`product` (name, description, price) VALUES ('Strawberry', 'A fresh strawberry', 0.20);
 INSERT INTO `protype`.`product` (name, description, price) VALUES ('Mango', 'A tropical mango', 1.00);
+
+UPDATE `protype`.`product` SET name = 'Red Apple', description = 'A Washington apple', price= '1.50'   WHERE id = 1;
 
 SELECT * FROM `protype`.`product`;
